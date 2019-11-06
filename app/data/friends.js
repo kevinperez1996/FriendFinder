64189bbf593b6580dec3ee7de86bd4c0611fce19
Kevin Perez <kevinperez996@outlook.com>
2019-11-06 18:17:31 -0500

    created routes and app listener

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+
+var friendsArray = [
+    {
+        name: "Kevin",
+        photo: "XXXXXXX",
+        scores: [4, 5, 2, 5, 3, 2, 1, 5, 3, 1]
+    }, 
+    {
+        name: "Bob", 
+        photo: "xxxxxx",
+        scores: [3, 5, 2, 2, 1, 3, 5, 4, 2, 1]
+    }, 
+    {
+        name: "Junior",
+        photo: "xxxxxxx",
+        scores: [4, 3, 2, 4, 5, 2, 2, 1, 4, 5]
+    }
+];
+
+module.exports = friendsArray;
